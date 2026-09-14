4e2e33f1ea805994f4bcdb765271622805ee5369
Christopher Wray <chris@sol.company>
2026-09-14 10:40:48 -0700

    Add Reviews component to showcase customer testimonials and integrate it into the homepage

---
+++ src/data/reviews.ts
@@ -0,0 +1,32 @@
+import type { Review } from '../types/reviews';
+
+export const googleMapsUrl =
+	'https://www.google.com/maps/place/Garden+Gate+Nursery/data=!4m2!3m1!1s0x0:0xf0f7d7d5303241d';
+
+export const reviews: Review[] = [
+	{
+		author: 'William Cross',
+		quote:
+			"Thank you for this beautiful tree, it's a perfect fit. I am pretty sure all of our trees came from your tree nursery.",
+	},
+	{
+		author: 'Kristina Marquardt',
+		quote:
+			'Garden Gate Nursery has numerous tree species to choose from. During my visit, I couldn’t help but notice how healthy all trees looked, and how the grounds were so well kept. The staff was amazing to work with, and provided sound insight when deciding on the perfect tree for my space. I highly recommend using Garden Gate for your next tree purchase 👍😊👍',
+	},
+	{
+		author: 'Kristi Smith',
+		quote:
+			'Such a great experience and so happy we found this nursery! All of the people there were super friendly and the dogwood tree that we purchased is very healthy and beautiful! We will be going back.',
+	},
+	{
+		author: 'Valerie Schatz',
+		quote:
+			"This is the 3rd year we've bought trees from Garden Gate Nursery, each time we've been impressed with the knowledge, friendliness and helpfulness; even telling us, in one instance that what we need we could get somewhere else. We look forward to when they open to the public each year. We've bought 10-12 trees so far and all are doing great. We won't buy a tree anywhere else! Thank you!\nPS Even if I wasn't going to buy a tree, I'd still go out there every year just for the experience, it's huge and amazing!",
+	},
+	{
+		author: 'Tiara Ders',
+		quote:
+			'Amazing quality trees, incredible customer service. There is a reason Garden Gate has all 5 star reviews. ⭐️⭐️⭐️⭐️⭐️. Thanks so much for all of your help and for these beautiful trees!',
+	},
+];
---
+++ src/types/reviews.ts
@@ -0,0 +1,6 @@
+export type Review = {
+	author: string;
+	quote: string;
+	date?: string;
+	relativeTime?: string;
+};
